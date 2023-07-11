b0abed9bfc4bdd99a0cd09f1c01949451722c428
Manuel Jeldes <135905713+babelk@users.noreply.github.com>
2023-07-11 14:04:47 -0400

    Add files via upload

---
+++ ListAssigment/src/com/codingdojo/listassigment/SinglyLinkedList.java
@@ -0,0 +1,54 @@
+package com.codingdojo.listassigment;
+
+public class SinglyLinkedList {
+    public Node head;
+
+    public SinglyLinkedList() {
+        this.head = null;
+    }
+
+    public void add(int value) {
+        Node newNode = new Node(value);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node runner = head;
+            while (runner.next != null) {
+                runner = runner.next;
+            }
+            runner.next = newNode;
+        }
+    }
+
+    public void remove() {
+        if (head == null) {
+            return;
+        }
+
+        if (head.next == null) {
+            head = null;
+            return;
+        }
+
+        Node current = head;
+        while (current.next.next != null) {
+            current = current.next;
+        }
+        current.next = null;
+    }
+
+    public void printValues() {
+        if (head == null) {
+            System.out.println("La lista está vacía.");
+            return;
+        }
+
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.value + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+    
+}
