224ab0b367d6bf6341821638c583c7a21a32e515
Manuel Jeldes <135905713+babelk@users.noreply.github.com>
2023-07-12 13:13:15 -0400

    Add files via upload

---
+++ ProyectosSpring/Cadenas/src/main/java/com/example/demo/CadenasApplication.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class CadenasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadenasApplication.class, args);
+	}
+@RequestMapping("/")
+public String saludo() {
+	return "Hola ¿como estas?";
+}
+
+@RequestMapping("/random")
+public String spring() {
+	return "Aprende SpringBoot, ya que es muy util.";
+}
+}
